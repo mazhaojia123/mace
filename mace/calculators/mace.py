Fix the issue of incorrect predict calculation after integrating LBFGS into mace.

mace/calculators/mace.py
@@ -24,15 +24,6 @@ from mace.tools.compile import prepare
 from mace.tools.scripts_utils import extract_model
 import random
 
-# def set_seed(seed: int):
-#     random.seed(seed)          # Python's built-in random module
-#     np.random.seed(seed)       # NumPy
-#     torch.manual_seed(seed)    # PyTorch (CPU and CUDA)
-#     torch.cuda.manual_seed(seed)
-#     torch.cuda.manual_seed_all(seed)  # If using multi-GPU
-#     torch.backends.cudnn.deterministic = True  # Ensures reproducibility
-#     torch.backends.cudnn.benchmark = False     # Ensures deterministic behavior
-#     torch.use_deterministic_algorithms(True)
 
 def get_model_dtype(model: torch.nn.Module) -> torch.dtype:
     """Get the dtype of the model"""
@@ -506,7 +497,8 @@ class MACECalculator(Calculator):
             num_atoms_arange, node_heads
         ]
         # compute_stress = not self.use_compile
-        compute_stress = True
+        # compute_stress = True
+        compute_stress = False
 
         # set_seed(0)
         out = self.models[0](
@@ -524,7 +516,59 @@ class MACECalculator(Calculator):
         # print(f'&&& training: {self.use_compile}')
         predictions["energy"] = out["energy"].unsqueeze(-1).detach()
         predictions["forces"] = out["forces"].detach()
-        predictions["stress"] = out["stress"].detach()
+        # predictions["stress"] = out["stress"].detach()
+
+        # print(f'&&& predictions["forces"] in predict: {predictions["forces"]}')
+
+        return predictions
+
+    def predict(self, atoms_list): 
+        predictions = {'energy': [], 'forces': []}
+
+        configs = [data.config_from_atoms(atoms, charges_key=self.charges_key) for atoms in atoms_list]
+        data_loader = torch_geometric.dataloader.DataLoader(
+            dataset=[
+                data.AtomicData.from_config(
+                    config, z_table=self.z_table, cutoff=self.r_max, heads=self.heads
+                )
+                for config in configs
+            ],
+            batch_size=len(atoms_list),
+            shuffle=False,
+            drop_last=False,
+        )
+
+        # get the first batch of data_loader
+        batch = next(iter(data_loader)).to(self.device)
+
+        # calculate node_e0
+        batch2 = self._clone_batch(batch)
+        node_heads = batch2["head"][batch2["batch"]]
+        num_atoms_arange = torch.arange(batch2["positions"].shape[0])
+        node_e0 = self.models[0].atomic_energies_fn(batch2["node_attrs"])[
+            num_atoms_arange, node_heads
+        ]
+        # compute_stress = not self.use_compile
+        # compute_stress = True
+        compute_stress = False
+
+        # set_seed(0)
+        out = self.models[0](
+            batch.to_dict(),
+            compute_stress=compute_stress, # TODO: DO WE NEED TO COMPUTE STRESS?
+            training=self.use_compile,
+        )
+        # print(f'&&& batch.positions: {batch["positions"]}')
+        # print(f'&&& batch.cell: {batch["cell"]}')
+        # print(f'&&& batch.stress: {batch["stress"]}')
+        # for k,v in batch.to_dict().items():
+        #     print(f'&&& batch.to_dict(): {k} {v}')
+        # print("=======")
+        # print(f'&&& out["forces"]: {out["forces"]}')
+        # print(f'&&& training: {self.use_compile}')
+        predictions["energy"] = out["energy"].unsqueeze(-1).detach()
+        predictions["forces"] = out["forces"].detach()
+        # predictions["stress"] = out["stress"].detach()
 
         # print(f'&&& predictions["forces"] in predict: {predictions["forces"]}')
 